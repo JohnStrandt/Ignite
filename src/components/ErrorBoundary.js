f14ab6a04e59e1a73debc9c0e21647bf529387f0
JohnStrandt <johnstrandt@Johns-MacBook-Pro.local>
2020-11-30 13:07:13 -0600

    Error Boundary v2

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,4 +1,4 @@
-import React, { Component, setState } from 'react'
+import React, { Component } from 'react'
 import styled from "styled-components";
 
 export class ErrorBoundary extends Component {
